4562bffa98a775207fecd401cf2c45dee85a8352
masa <masatoshi.sboard@gmail.com>
2020-09-21 08:16:38 +0900

    Done c

--- src/bin/c.rs
+++ src/bin/c.rs
@@ -1,7 +1,6 @@
 #[allow(unused_imports)]
 use proconio::marker::{Bytes, Chars, Usize1};
 use proconio::{fastout, input};
-use std::collections::HashMap;
 #[fastout]
 fn main() {
     input! {
@@ -9,42 +8,7 @@ fn main() {
     }
     let mut ans = 0;
     for i in 1..n {
-        let prime = trial_division(n - i);
-        let mut count = 1;
-        for (_, value) in prime.iter() {
-            count *= value + 1;
-        }
-        ans += count;
+        ans += (n - 1) / i;
     }
     println!("{}", ans);
-}
-
-fn trial_division(mut n: usize) -> HashMap<usize, usize> {
-    let mut primes = HashMap::new();
-    let mut i = 2;
-
-    //  n を root n 以下の整数で割り切れるまで割っていく
-    while i * i <= n {
-        while n % i == 0 {
-            n /= i;
-            if primes.contains_key(&i) {
-                let x = primes.get_mut(&i).unwrap();
-                *x += 1;
-            } else {
-                primes.insert(i, 1);
-            }
-        }
-        i += 1;
-    }
-
-    // 最後にnが素数になっている場合はそれ自身も素因数に含めて終了
-    if n > 1 {
-        if primes.contains_key(&n) {
-            let x = primes.get_mut(&n).unwrap();
-            *x += 1;
-        } else {
-            primes.insert(n, 1);
-        }
-    }
-    primes
 }
